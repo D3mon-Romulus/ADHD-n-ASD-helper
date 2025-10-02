1faab3b8c547c7049792e2586a84255a7060fa6d
D3mon-Romulus <134565976+D3mon-Romulus@users.noreply.github.com>
2025-10-02 14:03:42 +1000

    Delete kids app/info.js

--- kids app/info.js
+++
@@ -1,8 +0,0 @@
-function goBack() {
-  if (window.history.length > 1) {
-    window.history.back();
-  } else {
-    // If this is opened directly, you might want to redirect to your main app
-    window.location.href = 'index.html'; // Adjust this to your main app file
-  }
-}
